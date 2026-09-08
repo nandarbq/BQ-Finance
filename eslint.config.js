0bfe45949ef36f05e0c363d5cc6ed038729ea7ac
nandarbq <nandarassabiqi07@gmail.com>
2026-09-08 10:52:52 +0700

    chore(lint): add ESLint and Prettier tooling with lint/format scripts

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+
+export default [
+  { ignores: ["dist/**", "node_modules/**"] },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2022,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-undef": "off",
+    },
+  },
+];
